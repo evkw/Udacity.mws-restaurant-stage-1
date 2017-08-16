4bcf5a4fd464e789992e93721fbb378179888b4d
Evan Wallace <ewallace@berkleyre.com.au>
2017-08-16 15:17:49 +1000

    add static content to SW

--- sw.js
+++ sw.js
@@ -1,22 +1,45 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
-});var CACHE_NAME = 'restaurant-cache';
+}); var CACHE_NAME = 'restaurant-cache';
 var urlsToCache = [
   '/',
-  '/index.html',
-  '/restaurant.html',
-  '/css/*.css',
-  '/js/*.js',
-  '/img/*.jpg'
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
